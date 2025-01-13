a0b4a70df8f51d696c0c48677ecf5814196d57c8
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-01-13 18:31:42 +0100

    fix: Update spring security

--- src/main/java/com/login/firebase/FirebaseController.java
+++ src/main/java/com/login/firebase/FirebaseController.java
@@ -2,31 +2,57 @@ package com.login.firebase;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class FirebaseController {
 
-    @GetMapping("/login")
-    public String login(@RequestHeader("Authorization") String token) {
-        try {
-            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
-            return "User ID: " + decodedToken.getUid();
-        } catch (FirebaseAuthException e) {
-            return "Invalid token";
-        }
+    @Value("${firebase.apiKey}")
+    private String apiKey;
+
+    @Value("${firebase.authDomain}")
+    private String authDomain;
+
+    @Value("${firebase.projectId}")
+    private String projectId;
+
+    @Value("${firebase.storageBucket}")
+    private String storageBucket;
+
+    @Value("${firebase.messagingSenderId}")
+    private String messagingSenderId;
+
+    @Value("${firebase.appId}")
+    private String appId;
+
+    @Value("${firebase.measurementId}")
+    private String measurementId;
+
+    @GetMapping("/")
+    public String login(Model model) {
+        model.addAttribute("apiKey", apiKey);
+        model.addAttribute("authDomain", authDomain);
+        model.addAttribute("projectId", projectId);
+        model.addAttribute("storageBucket", storageBucket);
+        model.addAttribute("messagingSenderId", messagingSenderId);
+        model.addAttribute("appId", appId);
+        model.addAttribute("measurementId", measurementId);
+        return "index";
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestHeader("Authorization") String token) {
+    @GetMapping("/private")
+    @ResponseBody
+    public String ping(@RequestHeader("Authorization") String token) {
         try {
             FirebaseAuth.getInstance().verifyIdToken(token);
-            return "Hello World";
+            return "Pong";
         } catch (FirebaseAuthException e) {
             return "Unauthorized";
         }
