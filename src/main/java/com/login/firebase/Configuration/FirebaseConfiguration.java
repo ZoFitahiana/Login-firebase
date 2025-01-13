e55b439caf4e695aa702bff30452270d88b44160
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-01-13 20:27:18 +0100

    feat: add configuration

---
+++ src/main/java/com/login/firebase/Configuration/FirebaseConfiguration.java
@@ -0,0 +1,26 @@
+package com.login.firebase.Configuration;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfiguration {
+
+    @PostConstruct
+    public void init() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/google-services.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
